11e1ab9bc1a17fabcc144b20af0c9717cce0bcec
Ribhav <ribhav.soni.cse13@iitbhu.ac.in>
2015-04-16 15:20:39 +0530

    Update Titanic_survival.py

--- Titanic_survival_analysis/Titanic_survival.py
+++ Titanic_survival_analysis/Titanic_survival.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 
@@ -7,6 +8,8 @@
 import pandas as pd
 import numpy as np
 import pylab as P
+import matplotlib.pyplot as pyplot
+
 from sklearn import cross_validation
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
@@ -53,7 +56,7 @@ def cleanData(dataFrame):
     return dataFrame
 
 
-def printDataAnalyses(dataFrame):
+def printVisualizations(dataFrame):
     
     # Converting back to a numpy array, since we cannot apply scikit-learn's algorithms to a data frame object:
     
@@ -75,14 +78,14 @@ def printDataAnalyses(dataFrame):
     
     men_data = dataFrame[dataFrame['Gender']==1].values
     
-    print '\nPercentage of women on board', "%.2f" %(100*float(women_data.size)/float(train_data.size))
+    print '\nPercentage of women on board', "%.2f" %(100*float(women_data.size) / float(train_data.size))
     
-    print 'Percentage of men on board', "%.2f" %(100*float(men_data.size)/float(train_data.size))
+    print 'Percentage of men on board', "%.2f" %(100*float(men_data.size) / float(train_data.size))
     
     
-    print '\nPercentage of women survived : ', "%.2f" %(100*np.sum(women_data[:,0])/np.size(women_data[:,0]))
+    print '\nPercentage of women survived : ', "%.2f" %(100*np.sum(women_data[:,0]) / np.size(women_data[:,0]))
     
-    print 'Percentage of men survived : ', "%.2f \n" %(100*np.sum(men_data[:,0])/np.size(men_data[:,0]))
+    print 'Percentage of men survived : ', "%.2f \n" %(100*np.sum(men_data[:,0]) / np.size(men_data[:,0]))
     
     
     # Survival Percentage from each class :
@@ -95,15 +98,37 @@ def printDataAnalyses(dataFrame):
     survival_percentage = []
     
     for i in range(3):
-        survival_percentage.append ( 100 * np.sum(classes[i][:,0])/np.size(classes[i][:,0]) )
+        survival_percentage.append ( 100 * np.sum(classes[i][:,0]) / np.size(classes[i][:,0]) )
     
-    for i in range(3):
-          print 'Survival rate of people from class %d : %.2f' %(i+1,survival_percentage[i])
-
-
-def printVisualizations(dataFrame):
-        
+    ages=[]
+    aa=[]    
+    n_age=[]
+    for i in range (16):
+        aa.append( 5*i + 2.5 )
+        df = dataFrame[ ( dataFrame['Age'].astype(int) >= 5*i ) & ( dataFrame['Age'].astype(int) < 5*(i+1) ) ].values
+        ages.append( 100 * np.sum(df[:,0]) / np.size(df[:,0]) )
+        n_age.append(np.size(df[:,0]))
+    
+    
     # Some visualizations of the data:
+    
+    
+    
+    # No. of members of each class:
+    
+    dataFrame['Pclass'].hist(bins=3, range=(1,3), alpha = .5)
+    P.title(' Number of passengers of each class ')
+    P.ylabel(' Number of people ')
+    P.xlabel(' Class ')
+    P.show()
+    
+    # Survival Rate of different classes:
+    
+    pyplot.title(' Survival Rate with Passenger Class ')
+    pyplot.xlabel(' Passenger Class ')
+    pyplot.ylabel(' Survival Rate')
+    pyplot.bar( [ 1, 2, 3 ] , survival_percentage)
+    pyplot.show()
     
     # No. of people travelling with family members :
     
@@ -113,13 +138,40 @@ def printVisualizations(dataFrame):
     P.xlabel('Famly Size')
     P.show()
     
-    # No. of members of each class:
+    # No. of passengers of each age group on board :
     
-    dataFrame['Pclass'].hist(bins=3, range=(1,3), alpha = .5)
-    P.title(' Class of passengers ')
-    P.ylabel(' Number of people ')
-    P.xlabel(' Class ')
-    P.show()
+    pyplot.title(' Number of passengers in each age group ')
+    pyplot.xlabel(' Age ')
+    pyplot.ylabel(' Number of passengers ')
+    pyplot.bar(aa, n_age)
+    pyplot.show()    
+    
+    # Survival Chances with age of a person:
+    
+    pyplot.title(' Survival Chances with Age')
+    pyplot.xlabel(' Age ')
+    pyplot.ylabel(' Survival Rate ')
+    pyplot.bar(aa, ages)
+    pyplot.show()    
+    
+    
+    
+def visualizeRandomForestClassifier(X_train, y_train, X_test, y_test):
+
+    x = []
+    y = []
+    for i in range(1, 50):
+        x.append(2*i)
+        z = []
+        # Taking the average of 10 values for each value of no. of estimators:
+        for j in range(10):
+            z.append(RandomForestClassifier(n_estimators=2*i).fit(X_train,y_train).score(X_test, y_test))
+        y.append(float(sum(z))/len(z) if len(z) > 0 else float('nan'))
+        
+    pyplot.title('      Accuracy of Random Forest Classifier with no. of estimators ')
+    pyplot.ylabel(' Score ')
+    pyplot.xlabel(' Number of estimators ')
+    pyplot.plot(x, y)
 
 
 def trainModel(modelName, X_train, y_train):
@@ -145,7 +197,7 @@ def trainModel(modelName, X_train, y_train):
         
         # Applying Random Forest Classifier model to the training data:
     
-        randomForestCLassifierObject = RandomForestClassifier(n_estimators=100).fit(X_train,y_train)
+        randomForestCLassifierObject = RandomForestClassifier(n_estimators = 200).fit(X_train,y_train)
         
         return randomForestCLassifierObject
         
@@ -160,8 +212,6 @@ def main():
     trainDataFrame = readData('train.csv')
     
     train_df = cleanData(trainDataFrame)
-    
-    printDataAnalyses(train_df)
 
     printVisualizations(train_df)
     
@@ -170,19 +220,28 @@ def main():
     # Using cross-validation to check the performance of our models by reserving a part (40%) of the data for testing
     
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(train_data[0::,1::], train_data[0::,0], test_size=0.4, random_state=0)
-
+        
+    print 'Training Linear SVM model ... \n'
     
     Classifier1 = trainModel('LinearSVM', X_train, y_train)
     
+    print '\nAccuracy of linear SVM model : ', getClassifierScore(Classifier1, X_test, y_test), '\n\n'
+
+    print 'Training Decision Tree Classifier model ... \n'
+    
     Classifier2 = trainModel('DecisionTree', X_train, y_train)
+    
+    print '\nAccuracy of Decision Tree Classifier : ', getClassifierScore(Classifier2, X_test, y_test), '\n\n'
+    
+    print 'Training Random Forest Classifier model ... \n'
     
     Classifier3 = trainModel('RandomForest', X_train, y_train)
     
-    print '\nAccuracy of linear SVM model : ', getClassifierScore(Classifier1, X_test, y_test), '\n'
-    
-    print '\nAccuracy of Decision Tree Classifier : ', getClassifierScore(Classifier2, X_test, y_test), '\n'
-        
     print '\nAccuracy of Random Forest Classifier : ', getClassifierScore(Classifier3, X_test, y_test), '\n'
+    
+    print '\n\n Variation in Accuracy of Random Forest Classifier with no. of estimators : \n'
+    
+    visualizeRandomForestClassifier(X_train, y_train, X_test, y_test)
     
     
 if __name__ == '__main__':
